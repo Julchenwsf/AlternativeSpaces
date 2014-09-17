2bca1b6c7c62e39c1876c213108b6df41b160780
Manasa <manasa641@gmail.com>
2014-09-17 09:46:59 +0200

    Sql table for storing user data from Login or SignUp page

--- sql/Signuptabledef.sql
+++ sql/Signuptabledef.sql
@@ -1,9 +1,12 @@
-CREATE TABLE WebsiteUsers (
-userID int(9) NOT NULL auto_increment,
- fullname VARCHAR(50) NOT NULL,
- userName VARCHAR(40) NOT NULL,
- email VARCHAR(40) NOT NULL,
- pass VARCHAR(40) NOT NULL,
- PRIMARY KEY(userID)
-  );
-
+CREATE TABLE IF NOT EXISTS `member` (
+  `mem_id` int(9) NOT NULL AUTO_INCREMENT,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `fname` varchar(30) NOT NULL,
+  `lname` varchar(30) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `contact` varchar(30) NOT NULL,
+  `picture` varchar(100) NOT NULL,
+  `gender` varchar(10) NOT NULL,
+  PRIMARY KEY (`mem_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
