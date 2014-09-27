5964afe9b2723c3a10c5a590dbd46f85db365bfa
Manasa <manasa641@gmail.com>
2014-09-27 12:25:26 +0200

    Save file for inserting or saving data into users table

--- source/backend/db/Saveusers.php
+++ source/backend/db/Saveusers.php
@@ -9,7 +9,7 @@ $contact=$_POST['contact'];
 $pic=$_POST['pic'];
 $username=$_POST['username'];
 $password=$_POST['password'];
-mysql_query("INSERT INTO users(fname, lname, gender, address, contact, picture, username, password)VALUES('$fname', '$lname', '$mname', '$address', '$contact', '$pic', '$username', '$password')");
+mysql_query("INSERT INTO users(first_name, last_name, gender, address, contact, picture, username, password)VALUES('$fname', '$lname', '$mname', '$address', '$contact', '$pic', '$username', '$password')");
 header("location: index.php?remarks=success");
 mysql_close($con);
 ?>
