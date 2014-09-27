e29a47d8420689d6438003bb23bf574d4bdadc08
Julchen <Schneider_julia1@arcor.de>
2014-09-27 20:28:38 +0200

    Check login credentials for successful login. (bug fixing)

--- source/backend/db/DBLogin.php
+++ source/backend/db/DBLogin.php
@@ -9,8 +9,8 @@ include('DBConnection.php');
 // $username=$_POST['username'];
 // $password=$_POST['password'];
 
-$username = "Julchen";
-$password = "julia";
+$username = "XXX";
+$password = "xxx";
 //************************************************************************************************************
 // testing ob username or password or both are empty
 if ($username == "" or $password=="") {
@@ -19,17 +19,16 @@ if ($username == "" or $password=="") {
     $username = mysql_real_escape_string($username);
     $password = mysql_real_escape_string($password);
 
-    $result = mysql_query("SELECT * FROM users WHERE username='$username' AND password='$password'") or die(mysql_error());
-    while($row = mysql_fetch_row($result)) {
-        $dbusername = $row[1];
-        $dbpassword = $row[2];
-    }
+    $result = mysql_query("SELECT username, password FROM users WHERE username like '$username' AND password='$password'") or die(mysql_error());
+    $row = mysql_fetch_assoc($result);
+    $db_username = $row['username'];
+    $db_password = $row['password'];
 
-    if ($dbusername == "") {
+    if ($db_username == "") {
         echo "username or password incorrect";
     } else {
-        if ($dbpassword == $password) {
-            echo "login successful";
+        if ($db_password == $password) {
+            //echo "login successful";
             // Setting of session variables
             $_SESSION['user'] = $username;
 
@@ -44,6 +43,7 @@ if ($username == "" or $password=="") {
             echo "username or password incorrect";
         }
     }
+
 }
 
 ?>
