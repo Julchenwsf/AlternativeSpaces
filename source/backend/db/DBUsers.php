bfffa1d254d8b1aec640eac699d0e7e70f91e446
Julchen <Schneider_julia1@arcor.de>
2014-09-27 13:30:44 +0200

    Register form will be added to the database after pressing the submit button

--- source/backend/db/DBUsers.php
+++ source/backend/db/DBUsers.php
@@ -1,13 +1,24 @@
 <?php
+ini_set('display_errors',1);
+ini_set('display_startup_errors',1);
+error_reporting(-1);
+
+
+echo "test";
+
 include('DBConnection.php');
+
 $fname=$_POST['fname'];
 $lname=$_POST['lname'];
-$mname=$_POST['mname'];
 $address=$_POST['address'];
-$contact=$_POST['contact'];
+$gender=$_POST['gender'];
 $pic=$_POST['pic'];
 $username=$_POST['username'];
 $password=$_POST['password'];
-mysql_query("INSERT INTO users(first_name, last_name, gender, picture, username, password)VALUES('$fname', '$lname', '$mname', '$pic', '$username', '$password')") or die(mysql_error());
+$address = $_POST['address'];
+
+ mysql_query("INSERT INTO users(first_name, last_name, gender, picture, username, password, address)VALUES('$fname', '$lname', '$gender', '$pic', '$username', '$password', '$address')") or die(mysql_error());
+
 //header("location: index.php?remarks=success");
+
 ?>
