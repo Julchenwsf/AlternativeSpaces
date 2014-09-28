cb0cd9720a46115c076356f303ce7f04065a4c35
Manasa <manasa641@gmail.com>
2014-09-28 20:50:35 +0200

    For deleting the user profiles

--- source/deleteuser.php
+++ source/deleteuser.php
@@ -1,7 +1,7 @@
 <?php
 include("DBConnection.php");
 $id = $_GET['id'];
-//delete record based on selected id from deleteusers.php
+//delete record based on selected id from users.sql
 		$query = "Delete from users where id=$id";
 		mysql_query($query);
 		header("Location: DBUsers.php");
