61538ed100b561275dd2c9a1401e9573afa4be18
Valerij <valerijf@stud.ntnu.no>
2014-09-30 00:50:15 +0200

    Finished login from UI and login credential check

--- source/backend/forms/logform.php
+++ source/backend/forms/logform.php
@@ -2,20 +2,51 @@
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     include_once("../db/DBUsers.php");
-    //TODO: Run log in procedure
+    $status = checkLogin($_POST["username"], $_POST["password"]);
+    $response = array("success" => !empty($status), "response" => (!empty($status)) ? array() : array("Incorrect username/password"));
+    echo json_encode($response);
 
 } else if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-    //TODO: Make login form
     echo <<<EOT
     <div id="regTable">
         <div id="response"></div>
         <form id="submitTable" action="backend/forms/logform.php" method="post">
             <table>
                 <tr>
-                    <td>Login</td>
+                    <td colspan="2" id="center">Login</td>
+                </tr>
+                <tr>
+                    <td><input type="text" name="username" placeholder="Username" /></td>
+                    <td><input type="password" name="password" placeholder="Password" /></td>
+                </tr>
+                <tr>
+                    <td colspan="2" id="center"><input class="submitButton" name="logformSubmit" type="submit" value="Login" /></td>
                 </tr>
             </table>
         </form>
     </div>
+    <script type="text/javascript">
+    $('#submitTable').submit(function (ev) {
+        $.ajax({
+            type: "POST",
+            url: "backend/forms/logform.php",
+            data: $("#submitTable").serialize(),
+            dataType: "JSON",
+            success: function(data) {
+               if(data["success"]) {
+                   location.reload(true);
+               } else {
+                   var out = "";
+                   console.log(data["response"]);
+                   for(var error in data["response"]) {
+                       out += "<li>" + data["response"][error] + "</li>";
+                   }
+                   $("#response").html('<div class="error"><ul>' + out + "</ul></div>");
+               }
+            }
+        });
+        ev.preventDefault();
+    });
+    </script>
 EOT;
 } ?>
