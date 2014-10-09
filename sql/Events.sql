e1a416312ddd89b5177b8222a3bce0906ae18dd1
Manasa <manasa641@gmail.com>
2014-10-09 14:02:01 +0200

    For creating an event table with Date,time and loc

--- sql/Events.sql
+++ sql/Events.sql
@@ -2,10 +2,10 @@ CREATE TABLE Events (
     event_id int(9) NOT NULL AUTO_INCREMENT,
     eventname varchar(30) NOT NULL,
     Location varchar(30) NOT NULL,
-     DAYOFMONTH(date) NOT NULL,
-     MONTH NOT NULL,
-     YEAR NOT NULL,
-    HOUR NOT NULL,
-    MINUTE NOT NULL
+     DAY int(4) NOT NULL,
+     MONTH  int(4)NOT NULL,
+     YEAR int(4)NOT NULL,
+    HOUR int(4)NOT NULL,
+    MINUTE int(4)NOT NULL
     PRIMARY KEY (event_id)
 ) ENGINE=MyISAM
