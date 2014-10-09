385f7a8d9a5c4c32affe52bebe9ee3d3d0ba374a
Manasa <manasa641@gmail.com>
2014-10-09 17:43:44 +0200

    For creating an event table with Date,time and loc

--- sql/Events.sql
+++ sql/Events.sql
@@ -2,10 +2,9 @@ CREATE TABLE Events (
     event_id int(9) NOT NULL AUTO_INCREMENT,
     eventname varchar(30) NOT NULL,
     Location varchar(30) NOT NULL,
-     DAY int(4) NOT NULL,
+     DATE int(4) NOT NULL,
      MONTH  int(4)NOT NULL,
      YEAR int(4)NOT NULL,
-     No of people invited int(9)NOT NULL,
     HOUR int(4)NOT NULL,
     MINUTE int(4)NOT NULL
     PRIMARY KEY (event_id)
