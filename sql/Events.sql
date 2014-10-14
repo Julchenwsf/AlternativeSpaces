6abaf0e09ae556b60e66128f59883d3a7ae061f0
Manasa <manasa641@gmail.com>
2014-10-14 11:51:29 +0200

    edits made with date and time

--- sql/Events.sql
+++ sql/Events.sql
@@ -1,11 +1,7 @@
 CREATE TABLE events (
-    event_id int(9) NOT NULL AUTO_INCREMENT,
+    event_id int(9) NOT NULL AUTO_INCREMENT,,
+    event_time int(11) NOT NULL,
     eventname varchar(30) NOT NULL,
-    day int(4) NOT NULL,
-    month varchar(30) NOT NULL,
-    year int(6) NOT NULL,
-    hour int(9) NOT NULL,
-    minutes int(4) NOT NULL,
     no of people(9) NOT NULL
     description Text NOT NULL
     PRIMARY KEY (event_id)
