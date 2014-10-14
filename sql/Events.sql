13b8c5874fb3c73c0584d57c11ba70be9fc80c29
Manasa <manasa641@gmail.com>
2014-10-14 12:11:49 +0200

    edits made with syntax of table def

--- sql/Events.sql
+++ sql/Events.sql
@@ -1,9 +1,9 @@
 CREATE TABLE events (
     event_id int(9) NOT NULL AUTO_INCREMENT,
-    location Text NOT NULL,
+    location text NOT NULL,
     event_time int(11) NOT NULL,
     event_name varchar(30) NOT NULL,
     no_of_people int(9) NOT NULL,
-    description Text NOT NULL,
+    description text NOT NULL,
     PRIMARY KEY (event_id)
 ) ENGINE=MyISAM
