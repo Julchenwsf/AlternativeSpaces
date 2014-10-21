c1a98ceea6ef877d1966a5ad5b43d0574a5e2d46
Valerij Fredriksen <valerijf@stud.ntnu.no>
2014-10-21 19:56:01 +0200

    Removed SQL files. Use "SHOW CREATE TABLE [name]" instead.

--- sql/Events.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE events (
-    event_id int(9) NOT NULL AUTO_INCREMENT,
-    location text NOT NULL,
-    event_time int(11) NOT NULL,
-    event_name varchar(30) NOT NULL,
-    no_of_people int(9) NOT NULL,
-    description text NOT NULL,
-    PRIMARY KEY (event_id)
-) ENGINE=MyISAM
