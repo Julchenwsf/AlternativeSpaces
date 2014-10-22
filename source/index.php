dbac7e1cc276568664c3824ab916f32f7a3da735
freva <valerijf@stud.ntnu.no>
2014-10-22 17:33:59 +0200

    Frontpage is no longer supported, removed data.js (migrated to DB)

--- source/index.php
+++ source/index.php
@@ -1,23 +1,9 @@
 <?
 include_once("backend/PageBuilder.php");
 
-$content = '    <div id="eventMapSearch">
-                    <input type="text" id="input-interest-search" />
-                </div>
-
-                <div id="eventMapContainer">
-                    <div id="eventMap"></div>
-                    <div id="eventMapSidebar"></div>
-                </div>';
-
+$content = 'Hi';
 
 $pb = new PageBuilder("Index");
-$pb->addCSSImport("styles/token-input.css");
-$pb->addJSImport("https://maps.googleapis.com/maps/api/js?sensor=false");
-$pb->addJSImport("js/data.js");
-$pb->addJSImport("js/jquery.tokeninput.js");
-$pb->addJSImport("js/mapSearch.js");
-
 $pb->appendContent($content);
 
 echo $pb->toHTML();
