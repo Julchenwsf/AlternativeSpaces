a24c01da58343f11b7ca5402f0138ee18c80bd16
freva <valerijf@stud.ntnu.no>
2014-11-06 18:09:39 +0100

    Added location selector to upload image from IF the image does not have a preset location.

--- source/backend/forms/uploadphoto.php
+++ source/backend/forms/uploadphoto.php
@@ -6,14 +6,15 @@ include_once($_SERVER['DOCUMENT_ROOT'] .  "/backend/functions/log.php");
 
 if(isset($_FILES['image'])) {
     if(!isLoggedIn() && isset($_POST["username"]) && isset($_POST["password"])) login($_POST["username"], $_POST["password"]);
+    $coordinates = strlen($_POST["location"]) > 1 ? explode(" ", $_POST["location"]) : false;
 
-    $status = uploadImage($_POST["title"], $_POST["interests"], $_POST["description"], $_FILES["image"]);
+    $status = uploadImage($_POST["title"], $_POST["interests"], $_POST["description"], $_FILES["image"], $coordinates);
     $response = array("success" => is_numeric($status), "response" => $status);
     echo json_encode($response);
 }
 
 
-function uploadImage($title, $interests, $description, $image) {
+function uploadImage($title, $interests, $description, $image, $coordinates) {
     $errors = array();
     if(!isLoggedIn()) $errors[] = "You must be logged in to upload!";
 
@@ -25,7 +26,7 @@ function uploadImage($title, $interests, $description, $image) {
     if(!in_array($ext, $valid_exts)) $errors[] = "Invalid file type, only .jpeg and .jpg";
     if($image['error'] != 0) $errors[] = $fileErrors[$image["error"]];
 
-    $coordinates = getEXIFGPS($image["tmp_name"]);
+    if(!$coordinates) $coordinates = getEXIFGPS($image["tmp_name"]);
     if(!$coordinates) $errors[] = "Image has no coordinates, turn on your GPS";
     list($lat, $lng) = $coordinates;
 
@@ -45,7 +46,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     echo <<<EOT
     <div class="submitTable">
         <div id="response"></div>
-        <form id="newPhotoForm" enctype="multipart/form-data" action="javascript:;" method="post" onkeypress="return event.keyCode != 13;">
+        <form id="newPhotoForm" enctype="multipart/form-data" action="javascript:;" method="post">
             <table>
                 <tr>
                     <th colspan="2" id="center">Upload photo</th>
@@ -60,7 +61,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
                     <td colspan="2" id="center"><textarea rows="4" cols="55" name="description" placeholder="Description"></textarea></td>
                 </tr>
                 <tr>
-                    <td colspan="2"><input name="image" type="file" /></td>
+                    <td colspan="2"><input name="image" id="imageFile" type="file" /></td>
+                </tr>
+                <tr>
+                    <td colspan="2"><button type="button" onClick='openMapPicker()' class="submitButton right" id="locationButton" disabled>Map</button>
+                    <div style="width:360px;float:left"><input type="text" name="locationDisplay" placeholder="Location" id="location" disabled /></div>
+                    <input type="hidden" name="location" id="locationHidden"></td>
                 </tr>
                 <tr>
                   <td colspan="2" id="center"><button class="submitButton" name="submit" type="submit">Upload Photo</button></td>
@@ -80,7 +86,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
             dataType: "JSON",
             success: function(data) {
                 if(data["success"]) {
-                   window.location.replace("/map/photo/" + data["response"]);
+                   window.location.replace("/map/photos/" + data["response"]);
                 } else {
                    var out = "";
                    for(var error in data["response"]) {
@@ -92,6 +98,51 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
         });
         ev.preventDefault();
     });
+
+    $('#imageFile').on("change", function(){
+        var inputFile = $(this).prop("files")[0];
+        var formData = new FormData();
+        formData.append("image", inputFile);
+        $.ajax({
+            type: "POST",
+            url: "/backend/forms/getlocation.php",
+            data: formData,
+            processData: false,
+            contentType: false,
+            dataType: "JSON",
+            success: function(data) {
+                if(data["success"]) {
+                    setLocation(data["response"][0], data["response"][1]);
+                    $("#locationHidden").val(data["response"][0] + " " + data["response"][1]);
+                    $("#locationButton").prop("disabled", true);
+                } else {
+                    $("#locationButton").prop("disabled", false);
+                    $("#location").val("");
+                    $("#locationHidden").val("");
+                }
+            }
+        });
+    });
+
+    function openMapPicker() {
+        window.open("/backend/forms/maplookup.php",
+            "Select location", "width=620,height=460,location=no,menubar=no,resizable=no,status=no,toolbar=no");
+    }
+
+    function mapSelectionCallback(result) {
+        $("#locationHidden").val(result.lat() + " " + result.lng());
+        setLocation(result.lat(), result.lng());
+	}
+
+	function setLocation(lat, lng) {
+	    new google.maps.Geocoder().geocode({'latLng': new google.maps.LatLng(lat, lng)}, function(results, status) {
+            if (status == google.maps.GeocoderStatus.OK && results[0]) {
+                $("#location").val(results[0].formatted_address);
+            } else {
+                $("#location").val("Lat: " + lat + ", Lng: " + lng);
+            }
+        });
+    }
 
     $(document).ready(function() {
         interestsInput = $("#photoInterests");
